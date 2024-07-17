2af636a2c4126ceee2de0d73f5388b3247c884b9
muhali16 <muhammadali55214@gmail.com>
2024-07-17 22:39:38 +0700

    add mongoosh error handling

--- index.js
+++ index.js
@@ -22,24 +22,33 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+function wrapAsync(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((err) => next(err));
+  };
+}
+
 // home page
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 // show all products
-app.get("/products", async (req, res) => {
-  const { size } = req.query;
-  let products = await Product.find();
-  if (size) {
-    products = await Product.find({ size });
-  }
+app.get(
+  "/products",
+  wrapAsync(async (req, res) => {
+    const { size } = req.query;
+    let products = await Product.find();
+    if (size) {
+      products = await Product.find({ size });
+    }
 
-  if (size == "all") {
-    products = await Product.find();
-  }
-  res.render("products/index", { products, size: size });
-});
+    if (size == "all") {
+      products = await Product.find();
+    }
+    res.render("products/index", { products, size: size });
+  })
+);
 
 // create product page
 app.get("/products/create", (req, res) => {
@@ -47,36 +56,74 @@ app.get("/products/create", (req, res) => {
 });
 
 // create product
-app.post("/products", async (req, res) => {
-  const product = new Product(req.body);
-  await product.save();
-  res.redirect(`/products/${product._id}`);
-});
+app.post(
+  "/products",
+  wrapAsync(async (req, res) => {
+    const product = new Product(req.body);
+    await product.save();
+    res.redirect(`/products/${product._id}`);
+  })
+);
 
 // show product
-app.get("/products/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.render("products/show", { product });
-});
+app.get(
+  "/products/:id",
+  wrapAsync(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render("products/show", { product });
+  })
+);
 
 // edit product page
-app.get("/products/:id/edit", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.render("products/edit", { product });
-});
+app.get(
+  "/products/:id/edit",
+  wrapAsync(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render("products/edit", { product });
+  })
+);
 
 // update product
-app.put("/products/:id", async (req, res) => {
-  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    runValidators: true,
-  });
-  res.redirect(`/products/${product._id}`);
-});
+app.put(
+  "/products/:id",
+  wrapAsync(async (req, res) => {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+    });
+    res.redirect(`/products/${product._id}`);
+  })
+);
 
 // delete product
-app.delete("/products/:id", async (req, res) => {
-  await Product.findByIdAndDelete(req.params.id);
-  res.redirect("/products");
+app.delete(
+  "/products/:id",
+  wrapAsync(async (req, res) => {
+    await Product.findByIdAndDelete(req.params.id);
+    res.redirect("/products");
+  })
+);
+
+app.use((err, req, res, next) => {
+  if (err.name === "CastError") {
+    err.status = 404;
+    err.message = "Product not found";
+  }
+  if (err.name === "ValidationError") {
+    err.status = 400;
+    err.message = Object.values(err.errors)
+      .map((e) => e.message)
+      .join(", ");
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "something went wrong :(" } = err;
+  res.status(status).send(message);
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Page not found");
 });
 
 app.listen(3000, () => {
--- models/product.js
+++ models/product.js
@@ -3,20 +3,20 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Masukan nama produk anda."],
   },
   price: {
     type: Number,
-    required: true,
+    required: [true, "Masukan harga produk anda."],
   },
   size: {
     type: String,
-    required: true,
+    required: [true, "Masukan ukuran produk anda."],
     enum: ["S", "M", "L", "XL"],
   },
   color: {
     type: String,
-    required: true,
+    required: [true, "Masukan warna produk anda."],
   },
 });
 
